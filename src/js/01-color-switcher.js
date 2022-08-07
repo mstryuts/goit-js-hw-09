1e8417125284f12912454b42ac736bd5bf716a2c
maksim stryuts <maxim.stryuts@gmail.com>
2022-08-07 23:25:23 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+const DELAY = 1000;
 
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    timerID = setInterval(() => {
+    document.body.style.background = getRandomHexColor();  
+    }, DELAY)
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+
+stopBtn.addEventListener('click', () =>{
+    clearInterval(timerID);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+})
