62a74abb7b9206cbbaf3bc6d2263bd71965fc5ba
mstryuts <99408951+mstryuts@users.noreply.github.com>
2022-08-11 21:12:38 +0000

    Deploying to gh-pages from @ mstryuts/goit-js-hw-09@24ead61d5e243a93078fb1f83406df28d627596a 🚀

--- 01-color-switcher.86f382a6.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{timerID=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(timerID)}));
-//# sourceMappingURL=01-color-switcher.86f382a6.js.map
--- 01-color-switcher.9a11597a.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerID=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerID)}))}();
-//# sourceMappingURL=01-color-switcher.9a11597a.js.map
