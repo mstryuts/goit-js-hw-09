f4c484144cfc7a0437aa153fd6b1f07384b66d4d
mstryuts <99408951+mstryuts@users.noreply.github.com>
2022-08-11 20:44:02 +0000

    Deploying to gh-pages from @ mstryuts/goit-js-hw-09@823cd0032e4ad0531873b829505bb5d58ec01b1f 🚀

---
+++ 01-color-switcher.ba7033e5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){timerID=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerID),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.ba7033e5.js.map
