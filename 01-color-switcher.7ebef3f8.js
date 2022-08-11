b98b15f4da14851b0d035f2ec9db50913a92414a
mstryuts <99408951+mstryuts@users.noreply.github.com>
2022-08-11 21:06:45 +0000

    Deploying to gh-pages from @ mstryuts/goit-js-hw-09@a79cd656e442e987055955186e0720be11556020 🚀

--- 01-color-switcher.7ebef3f8.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(()=>{timerID=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(timerID),e.disabled=!0,t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.7ebef3f8.js.map
