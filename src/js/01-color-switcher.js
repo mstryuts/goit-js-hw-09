24ead61d5e243a93078fb1f83406df28d627596a
maksim stryuts <maxim.stryuts@gmail.com>
2022-08-12 00:11:49 +0300

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,16 +6,18 @@ function getRandomHexColor() {
 return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click', () => {
     timerID = setInterval(() => {
         document.body.style.background = getRandomHexColor();
     }, DELAY);
-   
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
 })
 
 stopBtn.addEventListener('click', () =>{
     clearInterval(timerID);
-   
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
 })
